7b73c1b448a16b44195d00172e67686c04fb65c0
Cyprinodon <dimitri.grabette@gmail.com>
2018-06-12 17:42:49 +0200

    Fixed a syntax error
    Added VARCHAR length to the code.

--- database/DB_creation.sql
+++ database/DB_creation.sql
@@ -1,9 +1,9 @@
 
 CREATE TABLE Comment (
                 ID INT NOT NULL,
-                author VARCHAR DEFAULT Visiteur anonyme NOT NULL,
-                content VARCHAR NOT NULL,
-                status VARCHAR DEFAULT À valider NOT NULL,
+                author VARCHAR(50) NOT NULL,
+                content TEXT NOT NULL,
+                status VARCHAR(50) NOT NULL,
                 creation_timestamp DATETIME NOT NULL,
                 PRIMARY KEY (ID)
 );
@@ -11,22 +11,22 @@ CREATE TABLE Comment (
 
 CREATE TABLE Article (
                 ID INT NOT NULL,
-                title VARCHAR DEFAULT Article title NOT NULL,
-                excerpt VARCHAR,
-                content VARCHAR,
+                title VARCHAR(100) NOT NULL,
+                excerpt TEXT,
+                content TEXT,
                 last_edit_timestamp DATETIME NOT NULL,
-                comment_id INT,
+                comment_id INT NOT NULL,
                 PRIMARY KEY (ID)
 );
 
 
 CREATE TABLE Moderator (
                 ID INT NOT NULL,
-                first_name VARCHAR DEFAULT Unnamed NOT NULL,
-                last_name VARCHAR DEFAULT Unnamed NOT NULL,
-                password VARCHAR NOT NULL,
+                first_name VARCHAR(50) NOT NULL,
+                last_name VARCHAR(50) NOT NULL,
+                password VARCHAR(30) NOT NULL,
                 signup_timestamp DATETIME NOT NULL,
-                article_id INT,
+                article_id INT NOT NULL,
                 PRIMARY KEY (ID)
 );
 
